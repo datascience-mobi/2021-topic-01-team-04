551c9dd7e876224d4d743f5d1282dc16f82aaff9
Hrafnkloo <82870173+Hrafnkloo@users.noreply.github.com>
2021-06-30 21:40:15 +0200

    d

--- Scripts/Initiator.R
+++ Scripts/Initiator.R
@@ -1,17 +1,21 @@
-cs <- as.logical(dlg_input(message = "Cleanslate?")$res)
-ps <- as.logical(dlg_input(message = "Re-initiate Presets?")$res)
-fn <- as.logical(dlg_input(message = "Re-initiate Functions?")$res)
-pl <- as.logical(dlg_input(message = "Re-initiate Preloader?")$res)
+library(svDialogs)
 
-if (cs) {
-  rm(list = ls(pos = ".GlobalEnv"), pos = ".GlobalEnv")
-}
-if (ps) {
-  source("Scripts/Presets.R")
-}
-if (fn) {
-  source("Scripts/Functions.R")
-}
-if (pl) {
-  source("Scripts/Preloader.R") 
+if (as.logical(dlg_input(message = "Re-initiate?")$res)) {
+  cs <- as.logical(dlg_input(message = "Cleanslate?")$res)
+  ps <- as.logical(dlg_input(message = "Re-initiate Presets?")$res)
+  fn <- as.logical(dlg_input(message = "Re-initiate Functions?")$res)
+  pl <- as.logical(dlg_input(message = "Re-initiate Preloader?")$res)
+  
+  if (cs) {
+    rm(list = ls(pos = ".GlobalEnv"), pos = ".GlobalEnv")
+  }
+  if (ps) {
+    source("Scripts/Presets.R")
+  }
+  if (fn) {
+    source("Scripts/Functions.R")
+  }
+  if (pl) {
+    source("Scripts/Preloader.R") 
+  }
 }
